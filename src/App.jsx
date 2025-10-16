a626a5026f736b649b2059e590f6e21bc174ebee
rajalishikhe <nahidujjamanraj@gmail.com>
2025-10-16 09:54:07 +0600

    Now I am solve the bugs of daisyUi

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,10 @@ function App() {
   return (
     <>
     
-      <h1 className='text-3xl text-amber-400'>Vite + React</h1>
+      <h1 className='text-3xl text-amber-400'>Vite + React</h1> 
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-info">Info</button>
+      
      
       
     </>
