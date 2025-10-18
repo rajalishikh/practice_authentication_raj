3a2ab1f157e0cb9a5dd23b919d692f2aa2410c45
rajalishikhe <nahidujjamanraj@gmail.com>
2025-10-18 11:32:04 +0600

    Now I am all so make login page

--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -2,7 +2,18 @@
 const Login = () => {
     return (
         <div>
-            <h2>Welcome To login page </h2>
+            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto mt-2">
+      <div className="card-body">
+        <form className="fieldset">
+          <label className="label">Email</label>
+          <input type="email" className="input" placeholder="Email" />
+          <label className="label">Password</label>
+          <input type="password" className="input" placeholder="Password" />
+          <div><a className="link link-hover">Forgot password?</a></div>
+          <button className="btn btn-neutral mt-4">Login</button>
+        </form>
+      </div>
+    </div>
             
             
         </div>
